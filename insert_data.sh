5f2666bdb543a1662e1dc7e4fa5cccc3110e4dc4
BlackTurbo Hi <sohandsome060@gmail.com>
2024-10-02 02:43:44 +0000

    Save progress: 1.1

--- insert_data.sh
+++ insert_data.sh
@@ -8,3 +8,32 @@ else
 fi
 
 # Do not change code above this line. Use the PSQL variable above to query your database.
+
+tail -n +2 "games.csv" | while IFS="," read -r year round winner opponent winner_goals opponent_goals
+do
+WINNER=$($PSQL "INSERT INTO teams(name) VALUES('$winner') ON CONFLICT (name) DO NOTHING;")
+if [[ "$WINNER" == "INSERT 0 1" ]]
+then
+echo "team inserted: $winner"
+fi
+OPPONENT=$($PSQL "INSERT INTO teams(name) VALUES('$opponent') ON CONFLICT (name) DO NOTHING")
+if [[ "$OPPONENT" == "INSERT 0 1" ]]
+then
+echo "team inserted: $opponent"
+fi
+
+WINNER_ID=$($PSQL "SELECT team_id FROM teams WHERE name = '$winner'")
+
+OPPONENT_ID=$($PSQL "SELECT team_id FROM teams WHERE name = '$opponent'")
+# Insert all values into the games table in one command
+RESULT=$($PSQL "INSERT INTO games(year, round, winner_id, opponent_id, winner_goals, opponent_goals) 
+VALUES('$year', '$round', $WINNER_ID, $OPPONENT_ID, $winner_goals, $opponent_goals)")
+
+if [[ "$RESULT" == "INSERT 0 1" ]]
+then
+    echo "Game inserted: year=$year, round=$round, winner_goals=$winner_goals, opponent_goals=$opponent_goals"
+else
+    echo "Failed to insert game: $RESULT"
+fi
+
+done
